3d174876fd5108edeb301a8bc335ab869178f321
Federico Zotti <probook.fezo@pm.me>
2019-12-16 21:02:19 +0100

    little fix

--- web-ui/web-ui.py
+++ web-ui/web-ui.py
@@ -19,54 +19,54 @@ dati_oggi, dati_voti, dati_compiti, dati_promemoria = gsd.get_dati()
 
 #BEGIN FLASK
 
-@app.route('/')
+@app.route("/")
 def page_default():
-	return redirect('/cosa-successo-oggi/')
+	return redirect("/cosa-successo-oggi/")
 
 
-@app.route('/voti/')
+@app.route("/voti/")
 def page_voti():
-	return render_template('voti.html', raw = dati_voti)
+	return render_template("voti.html", raw = dati_voti)
 
 
-@app.route('/cosa-successo-oggi/')
+@app.route("/cosa-successo-oggi/")
 def page_cosa_successo_oggi():
-	return render_template('cosa-successo-oggi.html', raw = dati_oggi)
+	return render_template("cosa-successo-oggi.html", raw = dati_oggi)
 
 
-@app.route('/compiti-asse-sett/')
+@app.route("/compiti-asse-sett/")
 def page_compiti_asse_sett():
 	oggi = datetime.date.today()
 	settimana = []
 	for i in range(7):
 		settimana.append(str(oggi - datetime.timedelta(days=i)))
 
-	return render_template('compiti-asse-sett.html', raw = dati_compiti, giorni = settimana)
+	return render_template("compiti-asse-sett.html", raw = dati_compiti, giorni = settimana)
 
 
-@app.route('/compiti-asse-data/<data_assegnati>/')
+@app.route("/compiti-asse-data/<data_assegnati>/")
 def page_compiti_asse_data(data_assegnati):
-	return render_template('compiti-asse-data.html', raw = dati_compiti, data = data_assegnati)
+	return render_template("compiti-asse-data.html", raw = dati_compiti, data = data_assegnati)
 
 
-@app.route('/data-prompt/', methods = ['POST', 'GET'])
+@app.route("/data-prompt/", methods = ["POST", "GET"])
 def page_data_prompt():
-	if request.method == 'POST':
+	if request.method == "POST":
 		data_assegnati = request.form["data_assegnati"]
-		return redirect('../compiti-asse-data/' + data_assegnati)
-	return render_template('data-prompt.html')
+		return redirect("../compiti-asse-data/" + data_assegnati)
+	return render_template("data-prompt.html")
 
 
-@app.route('/promemoria/')
+@app.route("/promemoria/")
 def promemoria():
 	oggi = str(datetime.date.today())
-	return render_template('promemoria.html', raw = dati_promemoria, oggi = oggi)
+	return render_template("promemoria.html", raw = dati_promemoria, oggi = oggi)
 
 
-@app.route('/update/')
+@app.route("/update/")
 def page_update():
 	gsd.update_dati()
-	return render_template('update.html')
+	return render_template("update.html")
 
 
 if __name__ == '__main__':
