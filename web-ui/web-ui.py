dbe1e653db955181339669f4360cfc40fac85384
Federico Zotti <probook.fezo@pm.me>
2019-12-20 14:11:25 +0100

    stuff

--- web-ui/web-ui.py
+++ web-ui/web-ui.py
@@ -1,75 +1,79 @@
 #!/usr/bin/env python3
-#web ui for the client
+# web ui for the client
 
 import datetime
-import os
 import pprint
+import os
+import sys
+from flask import Flask, redirect, url_for, render_template, request
+
 pp = pprint.PrettyPrinter(indent=4)
 
-import sys
 sys.path.append("../modules")
 import gestdati as gsd
 
-from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
-app.debug = True		#uncomment to develop using debug
+app.debug = True  # uncomment to develop using debug
 
 
 dati_oggi, dati_voti, dati_compiti, dati_promemoria = gsd.get_dati()
 
-#BEGIN FLASK
+
+# BEGIN FLASK
+
 
 @app.route("/")
 def page_default():
-	return redirect("/cosa-successo-oggi/")
+    return redirect("/cosa-successo-oggi/")
 
 
 @app.route("/voti/")
 def page_voti():
-	return render_template("voti.html", raw = dati_voti)
+    return render_template("voti.html", raw=dati_voti)
 
 
 @app.route("/cosa-successo-oggi/")
 def page_cosa_successo_oggi():
-	print(dati_oggi)
-	return render_template("cosa-successo-oggi.html", raw = dati_oggi)
+    return render_template("cosa-successo-oggi.html", raw=dati_oggi)
 
 
 @app.route("/compiti-asse-sett/")
 def page_compiti_asse_sett():
-	oggi = datetime.date.today()
-	settimana = []
-	for i in range(7):
-		settimana.append(str(oggi - datetime.timedelta(days=i)))
+    oggi = datetime.date.today()
+    settimana = []
+    for i in range(7):
+        settimana.append(str(oggi - datetime.timedelta(days=i)))
 
-	return render_template("compiti-asse-sett.html", raw = dati_compiti, giorni = settimana)
+    return render_template("compiti-asse-sett.html", raw=dati_compiti, giorni=settimana)
 
 
 @app.route("/compiti-asse-data/<data_assegnati>/")
 def page_compiti_asse_data(data_assegnati):
-	return render_template("compiti-asse-data.html", raw = dati_compiti, data = data_assegnati)
+    return render_template(
+        "compiti-asse-data.html", raw=dati_compiti, data=data_assegnati
+    )
 
 
-@app.route("/data-prompt/", methods = ["POST", "GET"])
+@app.route("/data-prompt/", methods=["POST", "GET"])
 def page_data_prompt():
-	if request.method == "POST":
-		data_assegnati = request.form["data_assegnati"]
-		return redirect("../compiti-asse-data/" + data_assegnati)
-	return render_template("data-prompt.html")
+    if request.method == "POST":
+        data_assegnati = request.form["data_assegnati"]
+        return redirect("../compiti-asse-data/" + data_assegnati)
+    return render_template("data-prompt.html")
 
 
 @app.route("/promemoria/")
 def promemoria():
-	oggi = str(datetime.date.today())
-	return render_template("promemoria.html", raw = dati_promemoria, oggi = oggi)
+    oggi = str(datetime.date.today())
+    return render_template("promemoria.html", raw=dati_promemoria, oggi=oggi)
 
 
 @app.route("/update/")
 def page_update():
-	gsd.update_dati()
-	gsd.get_dati()
-	return render_template("update.html")
+    gsd.update_dati()
+    dati_oggi, dati_voti, dati_compiti, dati_promemoria = gsd.get_dati()
+    return render_template("update.html")
 
 
-if __name__ == '__main__':
-	app.run()
+if __name__ == "__main__":
+    app.run()
